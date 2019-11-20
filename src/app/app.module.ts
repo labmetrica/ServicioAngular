8975abd5384303fbd1fec54eb29cf8de8b36aa77
AlexGranado <alejandro.granado@metrica.es>
2019-11-20 12:18:33 +0100

    horarios cambiar de turno WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, LOCALE_ID } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header/header.component';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import localeEs from '@angular/common/locales/es';
-import { registerLocaleData } from '@angular/common';
-import { ClientesComponent } from './clientes/clientes.component';
-import { ClienteService } from './clientes/cliente.service';
-import { GruposComponent } from './grupos/grupos.component';
-import { GruposService } from './grupos/grupos.service';
-import { Grupos2Component } from './grupos2/grupos2.component';
-import { Grupos2Service } from './grupos2/grupos2.service';
-import { Form2Component } from './grupos2/form2.component';
-import { Form3Component } from './grupos2/form3.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header/header.component";
+import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import localeEs from "@angular/common/locales/es";
+import { registerLocaleData } from "@angular/common";
+import { ClientesComponent } from "./clientes/clientes.component";
+import { ClienteService } from "./clientes/cliente.service";
+import { GruposComponent } from "./grupos/grupos.component";
+import { GruposService } from "./grupos/grupos.service";
+import { Grupos2Component } from "./grupos2/grupos2.component";
+import { Grupos2Service } from "./grupos2/grupos2.service";
+import { Form2Component } from "./grupos2/form2.component";
+import { Form3Component } from "./grupos2/form3.component";
 
-import { FormComponent } from './clientes/form.component';
-import { Form1Component } from './grupos/form1.component';
-import { FormsModule } from '@angular/forms';
-import { SubheaderComponent } from './subheader/SubheaderComponent';
-import { LoginComponent } from './login/login.component';
+import { FormComponent } from "./clientes/form.component";
+import { Form1Component } from "./grupos/form1.component";
+import { FormsModule } from "@angular/forms";
+import { SubheaderComponent } from "./subheader/SubheaderComponent";
+import { LoginComponent } from "./login/login.component";
 
-registerLocaleData(localeEs, 'es');
+registerLocaleData(localeEs, "es");
 
 const routes: Routes = [
-  { path: '', redirectTo: '/grupos', pathMatch: 'full' },
-  { path: 'clientes', component: ClientesComponent },
-  { path: 'grupos', component: GruposComponent },
-  { path: 'clientes/form', component: FormComponent },
-  { path: 'clientes/form/:id', component: FormComponent },
-  { path: 'grupos/form1', component: Form1Component },
-  { path: 'grupos/form1/:id', component: Form1Component },
-  { path: 'login', component: LoginComponent },
-  { path: 'grupos2', component: Grupos2Component },
-  { path: 'grupos2/form2', component: Form2Component },
-  { path: 'grupos2/form2/:id', component: Form2Component },
-  { path: 'grupos2/form3', component: Form3Component },
-  { path: 'grupos2/form3/:id', component: Form3Component }
+  { path: "", redirectTo: "/grupos2", pathMatch: "full" },
+  { path: "clientes", component: ClientesComponent },
+  { path: "grupos", component: GruposComponent },
+  { path: "clientes/form", component: FormComponent },
+  { path: "clientes/form/:id", component: FormComponent },
+  { path: "grupos/form1", component: Form1Component },
+  { path: "grupos/form1/:id", component: Form1Component },
+  { path: "login", component: LoginComponent },
+  { path: "grupos2", component: Grupos2Component },
+  { path: "grupos2/form2", component: Form2Component },
+  { path: "grupos2/form2/:id", component: Form2Component },
+  { path: "grupos2/form3", component: Form3Component },
+  { path: "grupos2/form3/:id", component: Form3Component }
 ];
 
 @NgModule({
@@ -64,7 +64,7 @@ const routes: Routes = [
   ],
   providers: [
     ClienteService,
-    { provide: LOCALE_ID, useValue: 'es' },
+    { provide: LOCALE_ID, useValue: "es" },
     GruposService,
     ,
     Grupos2Service
--- src/app/grupos/grupos.component.ts
+++ src/app/grupos/grupos.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Grupo } from './grupos';
-import { Grupos2Service } from '../grupos2/grupos2.service';
-import swal from 'sweetalert2';
-import { GroupedObservable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { Grupo } from "./grupos";
+import { Grupos2Service } from "../grupos2/grupos2.service";
+import swal from "sweetalert2";
+import { GroupedObservable } from "rxjs";
 
 @Component({
-  selector: 'app-grupos',
-  templateUrl: './grupos.component.html'
+  selector: "app-grupos",
+  templateUrl: "./grupos.component.html",
+  styleUrls: ["./grupos.component.css"]
 })
 export class GruposComponent implements OnInit {
   Grupos: Grupo[];
@@ -22,16 +23,16 @@ export class GruposComponent implements OnInit {
   delete(grupos: Grupo): void {
     swal
       .fire({
-        title: '¿Está seguro?',
+        title: "¿Está seguro?",
         text: `¿Seguro que desea eliminar al Grupo de las ${grupos.nombre}?`,
-        type: 'warning',
+        type: "warning",
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, eliminar!',
-        cancelButtonText: 'No, cancelar!',
-        confirmButtonClass: 'btn btn-success',
-        cancelButtonClass: 'btn btn-danger',
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, eliminar!",
+        cancelButtonText: "No, cancelar!",
+        confirmButtonClass: "btn btn-success",
+        cancelButtonClass: "btn btn-danger",
         buttonsStyling: false,
         reverseButtons: true
       })
@@ -40,9 +41,9 @@ export class GruposComponent implements OnInit {
           this.gruposService.delete(grupos.id).subscribe(response => {
             this.Grupos = this.Grupos.filter(cli => cli !== grupos);
             swal.fire(
-              'Grupo Eliminado!',
+              "Grupo Eliminado!",
               `Grupo de las ${grupos.nombre} eliminado con éxito.`,
-              'success'
+              "success"
             );
           });
         }
