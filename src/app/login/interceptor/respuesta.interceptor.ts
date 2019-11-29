fefdb31f9f5101814523ba07ffbabf84f04b2c87
AlexGranado <alejandro.granado@metrica.es>
2019-11-29 11:36:53 +0100

    interceptor de respuesta creado

---
+++ src/app/login/interceptor/respuesta.interceptor.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest
+} from "@angular/common/http";
+import { LoginService } from "../login.service";
+import { Observable, throwError, pipe } from "rxjs";
+import swal from "sweetalert2";
+import { catchError } from "rxjs/operators";
+import { Router } from "@angular/router";
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(private sesion: LoginService, private router: Router) {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError(e => {
+        if (e.status == 401) {
+          if (!this.sesion.sesionIniciada()) {
+            this.sesion.logout();
+          }
+          this.router.navigate(["/grupos2"]);
+        }
+        if (e.status == 403) {
+          swal.fire(
+            "Acceso denegado",
+            `No tienes acceso a este recurso ${this.sesion.usuario.username}`,
+            "warning"
+          );
+          this.router.navigate(["/grupos2"]);
+        }
+        return throwError(e);
+      })
+    );
+  }
+}
